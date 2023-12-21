5ecebe32ba2dfdc25171b632101307670b02ad51
ThomasVerbruggen <r0887895@student.thomasmore.be>
2023-12-21 16:16:05 +0100

    start form begining

--- product-service/src/main/java/fact/it/productservice/service/ProductService.java
+++ product-service/src/main/java/fact/it/productservice/service/ProductService.java
@@ -4,11 +4,9 @@ import fact.it.productservice.dto.ProductRequest;
 import fact.it.productservice.dto.ProductResponse;
 import fact.it.productservice.model.Product;
 import fact.it.productservice.repository.ProductRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -16,28 +14,6 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
-
-    @PostConstruct
-    public void loadData() {
-        if(productRepository.count() <= 0){
-            Product product = Product.builder()
-                    .skuCode("tube6in")
-                    .name("Stainless steel acid-resistant seamless tube OD 6 mm x 1 mm steel grade 316 / 316L 180cm")
-                    .price(BigDecimal.valueOf(7.50))
-                    .description("")
-                    .build();
-
-            Product product1 = Product.builder()
-                    .skuCode("beam10ft")
-                    .name("Wooden beam 10 feet long")
-                    .price(BigDecimal.valueOf(5.50))
-                    .description("Faux Wood Beams - 10 ft. Length & 10 in. Width Cast from natural wood beams with surface textures and wood-grain detail")
-                    .build();
-
-            productRepository.save(product);
-            productRepository.save(product1);
-        }
-    }
 
     public void createProduct(ProductRequest productRequest){
         Product product = Product.builder()
