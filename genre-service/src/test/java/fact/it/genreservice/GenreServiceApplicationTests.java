9bbd7d17f78e14d0369380dc63955bb87d81b889
JoppeKerkhofs <r0878799@student.thomasmore.be>
2023-12-21 16:34:45 +0100

    Added all services

---
+++ genre-service/src/test/java/fact/it/genreservice/GenreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.genreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- order-service/src/main/java/fact/it/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package fact.it.orderservice.repository;
-
-import fact.it.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
