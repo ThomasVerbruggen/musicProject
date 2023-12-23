cad993c2b9883f4cde998ac7aad72b2a44f0f0b3
tymov <r0878860@student.thomasmore.be>
2023-12-23 15:39:04 +0100

    changes

--- album-service/src/main/java/fact/it/albumservice/model/Album.java
+++ album-service/src/main/java/fact/it/albumservice/model/Album.java
@@ -3,22 +3,15 @@ package fact.it.albumservice.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "album")
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @Builder
 @Data
 public class Album {
-    @Id
     private String albumId;
     private String skuCode;
     private String title;
