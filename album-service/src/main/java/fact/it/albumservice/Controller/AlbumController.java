7334af90b57574f3af9414313a05eefccbe08888
Tymo Verhaegen <r0878860@student.thomasmore.be>
2023-12-27 17:12:51 +0100

    small fix

--- album-service/src/main/java/fact/it/albumservice/Controller/AlbumController.java
+++ album-service/src/main/java/fact/it/albumservice/Controller/AlbumController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/album")
+@RequestMapping("/api/albums")
 @RequiredArgsConstructor
 public class AlbumController {
 
