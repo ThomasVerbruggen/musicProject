233b34c0793b95b5e920b79332f84efda1860422
ThomasVerbruggen <r0887895@student.thomasmore.be>
2023-12-27 15:12:05 +0100

    fix

--- artist-service/src/main/java/fact/it/artistservice/controller/ArtistController.java
+++ artist-service/src/main/java/fact/it/artistservice/controller/ArtistController.java
@@ -10,7 +10,7 @@ import fact.it.artistservice.service.ArtistService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/artist")
+@RequestMapping("/api/artists")
 @RequiredArgsConstructor
 public class ArtistController {
 
